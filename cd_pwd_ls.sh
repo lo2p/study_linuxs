6f3eecf1cf494e76c9b1a9050c3899bb9b2f74d7
Min Ho <102045365+lo2p@users.noreply.github.com>
2025-07-14 18:03:06 +0900

    cd_pwd_ls

---
+++ cd_pwd_ls.sh
@@ -0,0 +1,12 @@
+# cd_pwd_ls
+
+[node@localhost ~]$ pwd
+/home/node
+[node@localhost ~]$ cd /home/node/Music/
+[node@localhost Music]$ ls
+[node@localhost Music]$ cd /home/node/Pictures/
+[node@localhost Pictures]$ ls -alh
+total 4.0K
+drwxr-xr-x.  2 node node    6 Jul 14 17:34 .
+drwx------. 14 node node 4.0K Jul 14 17:52 ..
+[node@localhost Pictures]$ 
