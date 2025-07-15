e4bbfbdc0490b39aa57dd79cafdc6ace86755061
Min Ho <102045365+lo2p@users.noreply.github.com>
2025-07-15 12:23:14 +0900

    second push

---
+++ codes/10_powershell.sh
@@ -0,0 +1,21 @@
+PS C:\Users\Administrator> cd C:\Develops\
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14   오후 6:06                Downloads
+d-----      2025-07-15   오전 9:34                VMs
+
+
+PS C:\Develops>
